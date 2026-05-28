62ab9a5fddd53969994c42e5b9d7d1edcca543a6
maggie <144709218+norombii@users.noreply.github.com>
2026-05-28 20:56:37 +0000

    outline

---
+++ Layout.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Layout {
+
+    public static ArrayList<Integer> triangle = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4));
+
+    public Layout(){
+
+    }
+
+    public ArrayList layers(int n) {
+
+        return triangle;
+    }
+
+}
+
+
+    
+
